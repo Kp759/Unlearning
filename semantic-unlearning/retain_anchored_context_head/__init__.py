c711755222207bf544a6fd94156766fe634191d3
Kaustubh <kaustubhofficial.21@gmail.com>
2026-09-05 14:47:45 -0400

    Export contextual head components

--- semantic-unlearning/retain_anchored_context_head/__init__.py
+++ semantic-unlearning/retain_anchored_context_head/__init__.py
@@ -1,9 +1,14 @@
-from .anchored_features import (
-    AnchoredFeatureMap,
-    wendland_c2_kernel,
+from .anchored_features import AnchoredFeatureMap, wendland_c2_kernel
+from .contextual_head import (
+    CorrectionDiagnostics,
+    FactIndexedLogitCorrection,
+    sequence_margin_loss,
 )
 
 __all__ = [
     "AnchoredFeatureMap",
+    "CorrectionDiagnostics",
+    "FactIndexedLogitCorrection",
+    "sequence_margin_loss",
     "wendland_c2_kernel",
 ]
